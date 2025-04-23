b542013b63c51beded8d67bda8df3eab55ee1b5d
Plinto Yura <plinto.yura@yandex.ru>
2025-04-24 01:34:56 +0600

    В сущность Статус добавлены константы

--- src/Entity/Status.php
+++ src/Entity/Status.php
@@ -8,6 +8,20 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: StatusRepository::class)]
 class Status
 {
+    const OPEN_ID      = 1;
+    const CLOSE_ID     = 2;
+    const CANCELLED_ID = 3;
+
+    const OPEN_NAME      = 'Открыто';
+    const CLOSE_NAME     = 'Закрыто';
+    const CANCELLED_NAME = 'Отменено';
+
+    const STATUSES = [
+        self::OPEN_ID      => self::OPEN_NAME,
+        self::CLOSE_ID     => self::CLOSE_NAME,
+        self::CANCELLED_ID => self::CANCELLED_NAME
+    ];
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
